6f136f0258b7fa3b1fe94d345f384f5ad1833767
Marwane <taroua.marwane@gmail.com>
2024-08-29 14:22:29 -0400

    update service card

--- app/components/service-card.tsx
+++ app/components/service-card.tsx
@@ -14,11 +14,13 @@ export default function ServiceCard({
 }: ServiceCardProps) {
   return (
     <div className="border bg-accent h-full border-secondary p-5 gap-8 items-center sm:w-[500px] m-auto">
-      <h3 className="text-3xl font-semibold tracking-wide">
-        {title.toUpperCase()}
-      </h3>
+      <div className="h-1/6 flex items-center">
+        <h3 className="text-3xl font-semibold tracking-wide">
+          {title.toUpperCase()}
+        </h3>
+      </div>
 
-      <div className="flex">
+      <div className="flex h-5/6">
         <div className="flex flex-col justify-evenly">
           <h3 className="text-3xl font-bold">$ {price}</h3>
           {featureList && (
